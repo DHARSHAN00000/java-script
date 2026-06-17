138a7d3c494173497194bc8f11a94d9b0155690a
DHARSHAN00000 <161416880+DHARSHAN00000@users.noreply.github.com>
2026-06-17 20:10:18 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,39 @@
+var a="dharshan"
+console.log(a);
+
+let age=23
+console.log(age);
+
+const company="Stackly"
+console.log(company);
+
+console.log("Employee name :" ,a);
+console.log("Employee age ;", age);
+console.log("Employee company ;" ,company);
+
+let b=alert("welcome to Employee portal")
+console.log(b);
+
+let c=confirm("Have you Completed todays's taask?")
+console.log(c);
+let result=(5>8);
+console.log(result);
+
+
+let d= prompt("Ask the Employee name")
+console.log(d);
+
+let e=document.writeln(" welcome naveen")
+console.log(e);
+
+let f="Task completed sucessfully"
+console.warn(f)
+
+
+let g=" task submission completed"
+console.error(g)
+
+
+
+
+
